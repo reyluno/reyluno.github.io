0fb2ec13e0909fdb84d17d1d71f834293bb05117
Nick <nickaldanarod@gmail.com>
2020-04-26 20:52:18 -0400

    backup

--- js/scroll.js
+++ js/scroll.js
@@ -5,10 +5,8 @@ $(window).scroll( function(){
   var topWindow = $(window).scrollTop();
   //multipl by 1.5 so the arrow will become transparent half-way up the page
   var topWindow = topWindow * 1.5;
-
   //get height of window
   var windowHeight = $(window).height();
-
   //set position as percentage of how far the user has scrolled
   var position = topWindow / windowHeight;
   //invert the percentage
