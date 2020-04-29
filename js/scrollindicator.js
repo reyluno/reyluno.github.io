10da98c08d5115df26ddbf7d121ba507d91ef8ba
Nick <nickaldanarod@gmail.com>
2020-04-28 20:40:10 -0400

    i hate js

--- js/scrollindicator.js
+++ js/scrollindicator.js
@@ -1,31 +1,10 @@
-var winHeight = $(window).height(),
-  docHeight = $(document).height();
-  max = docHeight - winHeight;
-
-$(progress).attr('max', max);
-
-var value = $(window).scrollTop();
-$(progress).attr('value', value);
 
 $(document).on('scroll', function() {
-  value = $(window).scrollTop();
-  progressBar.attr('value', value);
-});
+    if($(this).scrollTop()>=$('#about').position().top){
+        alert("scrolled.");
+    }
+})
 
-$(document).on('ready', function() {
-  var winHeight = $(window).height(),
-      docHeight = $(document).height(),
-      progressBar = $('progress'),
-      max, value;
-
-  /* Set the max scrollable area */
-  max = docHeight - winHeight;
-  progressBar.attr('max', max);
-
-  $(document).on('scroll', function(){
-     value = $(window).scrollTop();
-     progressBar.attr('value', value);
-  });
-});
-
-max = docHeight - winHeight; // equal to zero.
+function yourActionHere(){
+	alert("scrolled!");
+}
